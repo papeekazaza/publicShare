78e6e8977d1d46d9a21e521463248c5c24fa1ec8
DOGGA <sathanr56@email.nu.ac.th>
2016-03-16 22:55:28 +0700

    Bank ATM
    none

---
+++ MAIN.java
@@ -0,0 +1,11 @@
+public class MAIN
+{
+	// main method creates and runs the ATM
+	public static void main(String[] args)
+	{
+		ATM theATM = new ATM();
+		theATM.run();
+	} // end main
+}
+//12345, 54321, 1000.0, 1200.0		
+//98765, 56789, 200.0, 200.0
